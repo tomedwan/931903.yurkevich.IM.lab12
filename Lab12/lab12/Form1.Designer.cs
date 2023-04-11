ce820ad5dd97199559e411b5921e89de386fbf16
tomedwan <ilyauiv2000@gmail.com>
2023-04-11 21:05:18 +0700

    1

---
+++ Lab12/lab12/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace lab12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tt = new System.Windows.Forms.DataGridView();
+            this.number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PLD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.W = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.D = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.L = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PTS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.round = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tt
+            // 
+            this.tt.AllowUserToAddRows = false;
+            this.tt.AllowUserToDeleteRows = false;
+            this.tt.AllowUserToOrderColumns = true;
+            this.tt.AllowUserToResizeRows = false;
+            this.tt.BackgroundColor = System.Drawing.SystemColors.ControlDark;
+            this.tt.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tt.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            this.tt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tt.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.number,
+            this.name,
+            this.PLD,
+            this.W,
+            this.D,
+            this.L,
+            this.GS,
+            this.PTS});
+            this.tt.Location = new System.Drawing.Point(1, 1);
+            this.tt.Name = "tt";
+            this.tt.RowHeadersWidth = 51;
+            this.tt.RowTemplate.Height = 24;
+            this.tt.Size = new System.Drawing.Size(1296, 266);
+            this.tt.TabIndex = 3;
+            // 
+            // number
+            // 
+            this.number.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.number.HeaderText = "№";
+            this.number.MinimumWidth = 6;
+            this.number.Name = "number";
+            this.number.ReadOnly = true;
+            this.number.Width = 51;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Team";
+            this.name.MinimumWidth = 6;
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            this.name.Width = 125;
+            // 
+            // PLD
+            // 
+            this.PLD.HeaderText = "PLD";
+            this.PLD.MinimumWidth = 6;
+            this.PLD.Name = "PLD";
+            this.PLD.ReadOnly = true;
+            this.PLD.Width = 125;
+            // 
+            // W
+            // 
+            this.W.HeaderText = "WINS";
+            this.W.MinimumWidth = 6;
+            this.W.Name = "W";
+            this.W.ReadOnly = true;
+            this.W.Width = 125;
+            // 
+            // D
+            // 
+            this.D.HeaderText = "DRAWS";
+            this.D.MinimumWidth = 6;
+            this.D.Name = "D";
+            this.D.ReadOnly = true;
+            this.D.Width = 125;
+            // 
+            // L
+            // 
+            this.L.HeaderText = "LOSES";
+            this.L.MinimumWidth = 6;
+            this.L.Name = "L";
+            this.L.ReadOnly = true;
+            this.L.Width = 125;
+            // 
+            // GS
+            // 
+            this.GS.HeaderText = "GS";
+            this.GS.MinimumWidth = 6;
+            this.GS.Name = "GS";
+            this.GS.ReadOnly = true;
+            this.GS.Width = 125;
+            // 
+            // PTS
+            // 
+            this.PTS.HeaderText = "PTS";
+            this.PTS.MinimumWidth = 6;
+            this.PTS.Name = "PTS";
+            this.PTS.ReadOnly = true;
+            this.PTS.Width = 125;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(448, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(335, 84);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // round
+            // 
+            this.round.AutoSize = true;
+            this.round.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.round.Location = new System.Drawing.Point(60, 333);
+            this.round.Name = "round";
+            this.round.Size = new System.Drawing.Size(56, 25);
+            this.round.TabIndex = 5;
+            this.round.Text = "Круг";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(983, 308);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(983, 337);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(983, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1299, 431);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.round);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tt)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView tt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PLD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn W;
+        private System.Windows.Forms.DataGridViewTextBoxColumn D;
+        private System.Windows.Forms.DataGridViewTextBoxColumn L;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GS;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PTS;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label round;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Lab12/lab12/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace lab12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int roundNum = 0;   
+        Team[] teamList = new Team[8];
+
+        int[,,] scenario = new int[7, 4, 2]        
+        {{ { 0, 7 }, { 1, 6 }, { 2, 5 }, { 3, 4 }},
+          {{ 6, 0 }, { 7, 5 }, { 1, 4 }, { 2, 3 }},
+          {{ 0, 5 }, { 6, 4 }, { 7, 3 }, { 1, 2 }},
+          {{ 4, 0 }, { 5, 3 }, { 6, 2 }, { 7, 1 }},
+          {{ 0, 3 }, { 4, 2 }, { 5, 1 }, { 6, 7 }},
+          {{ 2, 0 }, { 3, 1 }, { 4, 7 }, { 5, 6 }},
+          {{ 0, 1 }, { 2, 7 }, { 3, 6 }, { 4, 5 } }}; 
+        
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            teamList[0] = new Team("Snowkids",   4.29,  1);
+            teamList[1] = new Team("Shadows", 3.33,  2);
+            teamList[2] = new Team("Xenon",   2.83,  3);
+            teamList[3] = new Team("Rikers",   2.81,  4);
+            teamList[4] = new Team("Pirates", 4.00, 5);
+            teamList[5] = new Team("Electro",    4.73, 6);
+            teamList[6] = new Team("Vambas",  2.99, 7);
+            teamList[7] = new Team("Lightning",   1.31, 8);
+            
+
+
+            func.setGrid(tt, teamList);
+            func.updateGrid(tt, teamList);
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            if (roundNum <= 6)
+            {
+                roundNum++;
+                round.Text = "Кругов пройдено: " + (roundNum) + "/7";
+
+                for (int i = 0; i < 4; i++)
+                {
+                    func.playMatch(teamList[scenario[roundNum-1,i,0]], 
+                    teamList[scenario[roundNum-1, i, 1]]);
+                }
+
+                func.sort(teamList);
+                func.getPos(teamList);
+                func.updateGrid(tt, teamList);
+            }
+            else if (roundNum == 7)
+            {
+                roundNum++;
+
+                func.winChecker(tt, teamList);
+
+                label1.Text = "Победили - " + teamList[0].name + " !!! ";
+
+                button1.Text = "Закончить турнир!";
+            }
+            else { Application.Exit(); }
+        }
+    }
+
+    public class Team
+    {
+        public int PTS = 0, W = 0, L = 0, 
+         D = 0, PLD = 0, POS = 0;
+        public string name;
+        public double lambda, GS = 0;
+        public Team(string name, double lambda, int POS)
+        {
+            this.name = name;
+            this.lambda = lambda;
+            this.POS = POS;
+        }
+    }
+
+    public class func
+    {
+        public static void setGrid(DataGridView grid, Team[] list)
+        { for (int i = 0; i < 8; i++) grid.Rows.Add(); } 
+
+        public static void updateGrid(DataGridView grid, Team[] list) 
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                grid.Rows[i].Cells[0].Value = list[i].POS;
+                grid.Rows[i].Cells[1].Value = list[i].name;
+                grid.Rows[i].Cells[2].Value = list[i].PLD;
+                grid.Rows[i].Cells[3].Value = list[i].W;
+                grid.Rows[i].Cells[4].Value = list[i].D;
+                grid.Rows[i].Cells[5].Value = list[i].L;
+                grid.Rows[i].Cells[6].Value = list[i].GS;
+                grid.Rows[i].Cells[7].Value = list[i].PTS;
+            }
+        } 
+
+        public static int poisson(Team team)
+        {
+            Random rnd = new Random();
+            int goals = 0;
+            double Summ = 0;
+
+            for (goals = -1; Summ >= -team.lambda; goals++) 
+                Summ+= Math.Log(rnd.NextDouble());
+            return goals;
+        } 
+
+        public static void playMatch(Team A, Team B)
+        {
+            int scoreA, scoreB;
+
+
+            scoreA = poisson(A);
+            scoreB = poisson(B);
+
+            A.GS += scoreA;
+            B.GS += scoreB;
+            A.PLD++; B.PLD++;
+
+
+            if (scoreA > scoreB)
+            {
+                A.PTS += 3; A.W++; B.L++;
+            } 
+            else if (scoreA < scoreB)
+            {
+                B.PTS += 3; B.W++; A.L++;
+            }
+            else
+            {
+                A.PTS++; B.PTS++;
+                A.D++; B.D++;
+            }
+
+        } 
+
+        public static void sort(Team[] teamList)
+        {
+            Team temp;
+            for (int i = 0; i < teamList.Length - 1; i++)
+            {
+                for (int j = 0; j < teamList.Length - i - 1; j++)
+                {
+                    if (teamList[j].PTS < teamList[j + 1].PTS)
+                    {
+                        temp = teamList[j];
+                        teamList[j] = teamList[j + 1];
+                        teamList[j + 1] = temp;
+                    }
+                }
+            }
+        } 
+
+        public static void getPos(Team[] teamList)
+         {
+            for (int i = 0; i < 8; i++) 
+                teamList[i].POS = i + 1;
+        } 
+
+
+        public static void winChecker(DataGridView grid, Team[] list) 
+        {                                        
+            if (list[0].PTS == list[1].PTS)
+            {
+                Team temp;
+                if (list[0].GS < list[1].GS)
+                {
+                    temp = list[0];
+                    list[0] = list[1];
+                    list[1] = temp;
+                }
+                func.updateGrid(grid, list);
+            }
+        }
+    }
+}
+
